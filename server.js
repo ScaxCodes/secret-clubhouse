6e04c9a6e35f867d8a89f336072e7a76271a81c7
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-05-08 05:38:49 +0200

    Add frontend for login and signup, add basic styling

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const server = express();
 
 const PORT = process.env.PORT || 8000;
 
+server.use(express.urlencoded({ extended: false }));
+server.use(express.static("public"));
+
 const path = require("node:path");
 server.set("views", path.join(__dirname, "views"));
 server.set("view engine", "ejs");
 
 server.get("/", (req, res) => {
   res.render("index");
+});
+
+server.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 server.listen(PORT, () =>
