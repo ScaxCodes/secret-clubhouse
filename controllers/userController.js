91a66d1ac115247a081f6652e70fa571457f23cf
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-05-08 06:35:43 +0200

    Store passwords as hashes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,17 @@
 const db = require("../db/queries");
+const bcrypt = require("bcryptjs");
 
 async function addUser(req, res) {
   const { firstname, lastname, username, password } = req.body;
   try {
-    await db.addUser({ firstname, lastname, username, password });
-    console.log("User signed up with:", { firstname, lastname, username });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.addUser({ firstname, lastname, username, hashedPassword });
+    console.log("User signed up with:", {
+      firstname,
+      lastname,
+      username,
+      hashedPassword,
+    });
     res.redirect("/");
   } catch (err) {
     res.status(500).send("Error signing up");
--- db/queries.js
+++ db/queries.js
@@ -4,8 +4,8 @@ const SQL_ADD_USER = /*sql*/ `
   INSERT INTO users (firstname, lastname, username, password) VALUES ($1, $2, $3, $4) RETURNING *`;
 
 async function addUser(user) {
-  const { firstname, lastname, username, password } = user;
-  const values = [firstname, lastname, username, password];
+  const { firstname, lastname, username, hashedPassword } = user;
+  const values = [firstname, lastname, username, hashedPassword];
   try {
     const { rows } = await pool.query(SQL_ADD_USER, values);
     return rows[0];
