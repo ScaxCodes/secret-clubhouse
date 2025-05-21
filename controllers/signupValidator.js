4819526e2bc1144f2c3cd60f6c49cf70d620baf0
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-05-21 15:29:40 +0200

    Use .body instead of .check for validator to be more explicit

--- controllers/signupValidator.js
+++ controllers/signupValidator.js
@@ -1,13 +1,12 @@
-const { check, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
 const validateSignup = [
-  check("username").notEmpty().withMessage("Username is required"),
-  check("firstname").notEmpty().withMessage("First Name is required"),
-  check("lastname").notEmpty().withMessage("Last Name is required"),
-  check("password").notEmpty().withMessage("Password is required"),
-
-  check("confirmPassword")
-    .exists()
+  body("username").notEmpty().withMessage("Username is required"),
+  body("firstname").notEmpty().withMessage("First Name is required"),
+  body("lastname").notEmpty().withMessage("Last Name is required"),
+  body("password").notEmpty().withMessage("Password is required"),
+  body("confirmPassword")
+    .notEmpty()
     .withMessage("Confirm password is required")
     .custom((value, { req }) => {
       if (value !== req.body.password) {
@@ -15,7 +14,6 @@ const validateSignup = [
       }
       return true;
     }),
-
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
